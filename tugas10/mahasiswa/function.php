2935a22d770f4cd1796ec2fdbc9ad612e8cf8efe
yannaelshofa <yannaelshofa@gmail.com>
2020-09-06 23:46:03 +0700

    latihan10

---
+++ tugas10/mahasiswa/function.php
@@ -0,0 +1,15 @@
+<?php
+function koneksi()
+{
+  return  mysqli_connect("localhost", "root", "", "yanna-unpas");
+}
+function query($query)
+{
+  $conn = koneksi();
+  $result = mysqli_query($conn, $query);
+  $rows = [];
+  while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+  }
+  return $rows;
+}
---
+++ tugas10/mahasiswa/index.php
@@ -0,0 +1,41 @@
+<?php
+
+
+//var_dump($query);
+require "function.php";
+$mahasiswa = query("select * from mahasiswa");
+//var_dump($rows);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Data Mahasiswa</title>
+</head>
+
+<body>
+  <h1>Data Mahasiswa</h1>
+  <table border="1">
+    <tr>
+      <th>#</th>
+      <th>Nama</th>
+      <th>nrp</th>
+      <th>email</th>
+    </tr>
+    <?php $i = 1;
+    foreach ($mahasiswa as $m) : ?>
+      <tr>
+        <td><?= $i++; ?></td>
+        <td><?= $m['nama']; ?></td>
+        <td><?= $m['nrp']; ?></td>
+        <td><?= $m['email']; ?></td>
+      </tr>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
